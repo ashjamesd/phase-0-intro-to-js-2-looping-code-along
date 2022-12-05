48dd3cbff1c8b27ed3d513177774042f7143bc73
Ash <ashjames.dean@gmail.com>
2022-12-05 12:16:02 -0500

    commit tree 09dfa01e183fc4e55dcfc8310a0713a2ae18ced7

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function writeCards (names,event){
+    const messages = [];
+    for (let i = 0; i <names.length; i++){
+        const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+        messages.push(message);
+    }
+    return messages;
+}
+function countDown (){
+    var answ = '';
+    let i=0;
+    while (--i>=0){
+        answ = answ + i + '';
+        i--
+    }
+    return answ;
+}
+console.log(coundown(10));
